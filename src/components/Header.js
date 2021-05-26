aadba993e1f1845551a4b63b6cd8165e5dc2f044
cr1m5onk1ng <mirco.cardinale.91@gmail.com>
2021-05-26 15:33:56 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ function Header(){
         <div className="header">
             <div className="header__left">
                     <h2>Mirco Cardinale</h2>
-                    <p>Developer, NLP, ML, Android</p>
+                    <p>Developer, NLP, ML, Mobile</p>
             </div>
             <div className="header__right">
                 <Link to="/">
